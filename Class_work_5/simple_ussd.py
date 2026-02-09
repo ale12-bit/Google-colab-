3c9999a17aad022df68b616f797c919775e9db91
Alex <educateale12@gmail.com>
2026-02-09 13:12:32 +0300

    Add simple USSD menu for deals selection

---
+++ Class_work_5/simple_ussd.py
@@ -0,0 +1,22 @@
+print('Welcome to home of amazing deals')
+print('0 Fuliza')
+print('1 Mpesa')
+print('2 Buy airtime')
+print('3 Okoa')
+print('4 Data deals')
+print('5 Bonga Deals')
+choice=int(input('select your prefferd deal; '))
+choose=int(choice)
+if choose==0:
+    print(' Fuliza')
+elif choose==1:
+   print(' Welcome to Mpesa')
+elif choose==2:
+   print(' Welcome to Buy airtime')
+elif choose==3:
+   print(' Welcome to Okoa')
+elif choose==4:
+   print(' Weclome to Data deals')
+elif choose==5:
+   print(' Welcome to Bonga deals')
+else: print('invalid choice')  
