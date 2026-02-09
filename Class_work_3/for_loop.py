a3a5e8278c581fe40ef791c704bf3f054c17aba2
Alex <educateale12@gmail.com>
2026-02-09 13:04:30 +0300

    Add for loop examples for fruits and number checks

---
+++ Class_work_3/for_loop.py
@@ -0,0 +1,14 @@
+fruits=['mangoes','apples','avocados','oranges']
+for i in fruits:
+    print(f' favourite {i}')
+print(type(fruits))
+for y in range(10):
+    print(y)
+for a in range(1 , 100, 10):
+    print(a)
+for num in range(1,100):
+    if num%2==0:
+        print(f'{num} is even')
+for num in range(1,100):
+    if num%2==1:
+        print(f'{num} is odd')
